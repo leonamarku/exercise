7c8032e97102365308742e747252aa02f305ca86
leonamarku <145600948+leonamarku@users.noreply.github.com>
2023-10-19 20:15:48 +0200

    Add files via upload

---
+++ Exercise1Bis.py
@@ -0,0 +1,46 @@
+ def __init__(self, name, surname, age, address, phone_number):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.address = address
+        self.phone_number = phone_number
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_surname(self):
+        return self.surname
+
+    def set_surname(self, surname):
+        self.surname = surname
+
+    def get_age(self):
+        return self.age
+
+    def set_age(self, age):
+        self.age = age
+
+    def get_address(self):
+        return self.address
+
+    def set_address(self, address):
+        self.address = address
+
+    def get_phone_number(self):
+        return self.phone_number
+
+    def set_phone_number(self, phone_number):
+        self.phone_number = phone_number
+
+    def print_info(self):
+        print(f"Name: {self.name}")
+        print(f"Surname: {self.surname}")
+        print(f"Age: {self.age}")
+        print(f"Address: {self.address}")
+        print(f"Phone Number: {self.phone_number}")
+
+person = Person("Era", "Aliu", 20, "Avd. Loyola University", "666-333-111")
+person.print_info()
